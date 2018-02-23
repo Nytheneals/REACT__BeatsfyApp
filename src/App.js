b8dc46472a8fd11046c9626d47ce972e48d268bb
Nytheneals <nythes@gmail.com>
2018-02-22 23:50:22 -0500

    Worked on CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Filter from './Components/Filter';
 import Aggregate from './Components/Aggregate';
 import Playlist from './Components/Playlist';
+import './App.css';
 
 class App extends Component {
   render() {
