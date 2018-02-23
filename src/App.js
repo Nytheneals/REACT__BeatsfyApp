70796495fa87e6dd57f5d08469ce517360141e49
Nytheneals <nythes@gmail.com>
2018-02-23 00:29:59 -0500

    Added Fake Data and State to App

--- src/App.js
+++ src/App.js
@@ -4,13 +4,35 @@ import Aggregate from './Components/Aggregate';
 import Playlist from './Components/Playlist';
 import './App.css';
 
+// FAKE DATA
+const fakeServerData = {
+  user: {
+    name: 'Nathane',
+  },
+};
+
+// PLAYLIST LIST DATA
+
+// START OF APP COMPONENT
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      serverData: {},
+    };
+  }
+
+  // LIFE CYCLE THIS RUNS AFTER THE COMPONENT MOUNTS
+  componentDidMount() {
+    this.setState({ serverData: fakeServerData });
+  }
+
   render() {
-    // const name = 'Wakanda';
     return (
       <div className="App">
         <h1>Kandafy</h1>
         <h4>Music for Wakandans</h4>
+        <h4>{this.state.serverData.user && this.state.serverData.user.name}'s Playlist</h4>
         <Aggregate />
         <Aggregate />
         <Filter />
